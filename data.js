fda918cdc7fd1579f83e0a1230d505434a173945
nadine763 <nadine96jodie@gmail.com>
2020-11-05 15:45:23 +0200

    Data

---
+++ data.js
@@ -0,0 +1,27 @@
+let full_name = "Nadine Jodie Fortuin";
+document.write(full_name + "<br/>");
+document.write(typeof full_name + "<br/><br/>");
+
+let x = "Nadine Jodie Fortuin" + 24;
+document.write(x + "<br/>");
+document.write(typeof x + "<br/><br/>");
+
+let what_you_doing = "I am currently doing the coding exercise for Data Types";
+document.write(what_you_doing + "<br/>");
+document.write(typeof what_you_doing + "<br/><br/>");
+
+let age = 24;
+document.write(age + "<br/>");
+document.write(typeof age + "<br/><br/>");
+
+let greater_than = 10 > 100;
+document.write(greater_than + "<br/>");
+document.write(typeof greater_than + "<br/>");
+
+let my_age = ; 
+document.write(my_age + "<br/>");
+document.write(typeof(my_age) + "<br/><br/>");
+
+let name = null;
+document.write(name + "<br/>");
+document.write(typeof(name) + "<br/><br/>");
